71e4273945fbb3eac080e14d540853b352dc74c1
marufmustarmoon <marufmustarmoon@gmail.com>
2024-10-02 21:34:23 +0600

    leetcode question 238 solved

---
+++ problem238_Leetcode_product_of_array_except_self.py
@@ -0,0 +1,27 @@
+def productExceptSelf(nums):
+    n = len(nums)
+    result = [1] * n
+
+   
+    prefix = 1
+    for i in range(n):
+        result[i] = prefix
+        prefix *= nums[i]
+
+   
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        result[i] *= suffix
+        suffix *= nums[i]
+
+    return result
+
+
+
+
+
+
+
+nums = [1,2,3,4]
+result = productExceptSelf(nums)
+print(result)
